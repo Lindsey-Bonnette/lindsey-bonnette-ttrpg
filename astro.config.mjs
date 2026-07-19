09549e0180cb3707323db6594b885fef8234f350
Lindsey-Bonnette <lindsey.r.bonnette@proton.me>
2026-07-19 14:59:08 -0500

    Set output to static for GitHub Pages
    Added output configuration for static site generation.

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,9 +4,9 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import { defineConfig, fontProviders } from 'astro/config';
 
-// https://astro.build/config
 export default defineConfig({
 	site: 'https://www.lindseybonnetterpgpublishing.com',
+	output: 'static', // Ensure static output for GitHub Pages
 	integrations: [mdx(), sitemap()],
 	fonts: [
 		{
